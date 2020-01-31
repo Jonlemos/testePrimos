1c491a7586de42f3f69e1160d3f20c4e070996fd
jonlemos <lemos.anjos@gmail.com>
2020-01-31 18:21:07 -0300

    finalizado js

--- js/script.js
+++ js/script.js
@@ -1,47 +1,32 @@
 (function teste(){
 
 
-    var letterFist = document.getElementById('letterFist')
-    var letterSec = document.getElementById('letterSec')
+    var firstChar = document.getElementById('firstChar')
+    var secChar = document.getElementById('secChar')
     var sendLetter =document.getElementById('sendLetter')
     let alphaValue = {}
 
 
-    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','x','y','z']
+    var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
     alphabet.forEach((char, index)=>{
         alphaValue[char] = index + 1
-        alphaValue[char.toUpperCase()]= index + 27
-
-
-
-        
+        alphaValue[char.toUpperCase()]= index + alphabet.length + 1
     })
 
     console.log(alphaValue)
 
     var isPrime = (a , b) => {
-        if(a % b === 0){
+        if(alphaValue[a] % alphaValue[b] === 0){
             return   console.log(`É primo`)
         }
         console.log('num é')
-
-
     }
 
     sendLetter.addEventListener('click', function(){
-
-        alphabet.forEach((saysPrime) =>{
-
-            if(saysPrime === letterSec.value){
-                console.log(saysPrime[alphaValue])
-            }
-
-        })
-        
-        // console.log(letterFist.value)
-        // console.log(letterSec.value)
-
+        var firstCharValue = firstChar.value
+        var secCharValue = secChar.value
+        isPrime(firstCharValue, secCharValue)
     })
     
 
